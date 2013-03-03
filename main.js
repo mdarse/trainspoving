1e048b18e13a4d9e20607d0945ffe3976f432652
Pauline Lherbette <lherbette.pauline@gmail.com>
2013-03-03 22:28:56 +0100

    Add code for RFID listener and Send information to POV

--- main.js
+++ main.js
@@ -20,9 +20,17 @@ var exitFunction = function(event) {
 var onKarotzConnect = function(data) {
     karotz.button.addListener(buttonListener);
     var server = new Server();
-    karotz.rfid.addListener( 
+    var timeBeforeTrain = karotz.rfid.addListener( 
         server = function sendInformation(data);
     );
+
+    var display = new Display();
+    //If the time before train is false we have not find the train else we can launch POV
+    if (timeBeforeTrain == false) {
+        karotz.tts.start("Je suis désolé je n'ai trouvé aucun train correspondant.", "fr");
+    }else{
+        display = function sendInformation(timeBeforeTrain);
+    };
 }
 
 karotz.connectAndStart(karotz_ip, 9123, onKarotzConnect, {});
