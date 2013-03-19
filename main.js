aa91bd2636a83b12b741caadfe725b2d4091c056
Mathieu Darse <mattdarse@gmail.com>
2013-03-19 18:52:59 +0100

    Remake app basics

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
 include("util.js");
 
-var karotz_ip="localhost";
+(function(_log) {
+  var host = 'localhost';
 
-var buttonListener = function(event) {
-    if (event == "DOUBLE") {
-        karotz.tts.stop();
-        exit();
+  function log(message) {
+    var now = new Date(),
+        data = "[" + now.toJSON() + "] ";
+    data += (typeof(message) == 'object') ? JSON.stringify(message) : message;
+    if (host == 'localhost')
+      // Native
+      karotz.serial.write(data + '\n');
+    else
+      // Java KarotzVM
+      _log(message);
+  }
     }
-    return true;
-}
 
-var exitFunction = function(event) {
-    if((event == "CANCELLED") || (event == "TERMINATED")) {
-        exit();
+  karotz.boot = function(callback, data) {
+    try {
+      var boot = function(data) {
+        karotz.serial.open("/dev/ttyGS0", 9600);
+        log("-------------------");
+        try { callback(data); }
+        catch (e) { log(e); exit(); }
+      };
+      karotz.connect(host, 9123);
+      karotz.start(boot, data || { "foo": "bar" });
+    } catch (e){
+      log(e);
     }
-    return true;
-}
+  };
 
-var onKarotzConnect = function(data) {
-    karotz.button.addListener(buttonListener);
-    var server = new Server();
-    var timeBeforeTrain = karotz.rfid.addListener( 
-        server = function sendInformation(data);
-    );
-
-    var display = new Display();
-    //If the time before train is false we have not find the train else we can launch POV
-    if (timeBeforeTrain == false) {
-        karotz.tts.start("Je suis désolé je n'ai trouvé aucun train correspondant.", "fr");
-    }else{
-        display = function sendInformation(timeBeforeTrain);
+  var App = function() {
+  };
+  App.prototype.start = function() {
+    var that = this;
+    // add listeners
+    var rfidListener = function(data) {
+      karotz.rfid.addListener(rfidListener); // Fix listener called only first time
+      that.onRfid(data);
     };
-}
+    karotz.rfid.addListener(rfidListener);
+  };
+  App.prototype.onRfid = function(data) {
+    log("RFID " + data.id);
+  };
+  App.prototype.hello = function() {
+    log("Hello World!");
+  };
 
-karotz.connectAndStart(karotz_ip, 9123, onKarotzConnect, {});
+  karotz.boot(function(data) {
+    // Start app
+    var app = new App();
+    app.start();
+  });
+
+
+
+})(log);
